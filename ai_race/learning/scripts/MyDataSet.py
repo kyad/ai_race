3978fc6797e9e6ba4d0d190dbb55265f845beede
kyad <53517611+kyad@users.noreply.github.com>
2020-12-31 06:41:51 +0000

    Use PEP8 indent

--- ai_race/learning/scripts/MyDataSet.py
+++ ai_race/learning/scripts/MyDataSet.py
@@ -9,20 +9,20 @@ LABEL_IDX = 2
 IMG_IDX = 1
 
 class MyDataset(Dataset):
-   def __init__(self,image_dataframe, transform=None):
-      self.image_dataframe = image_dataframe
-      self.transform = transform
+    def __init__(self,image_dataframe, transform=None):
+        self.image_dataframe = image_dataframe
+        self.transform = transform
       
-   def __len__(self):
-      return len(self.image_dataframe)
+    def __len__(self):
+        return len(self.image_dataframe)
    
-   def __getitem__(self, idx):
-      label =[0]*3
-      label=self.image_dataframe.iat[idx, LABEL_IDX]
-      img_name = self.image_dataframe.iat[idx, IMG_IDX]
+    def __getitem__(self, idx):
+        label =[0]*3
+        label=self.image_dataframe.iat[idx, LABEL_IDX]
+        img_name = self.image_dataframe.iat[idx, IMG_IDX]
       
-      image = cv2.imread(img_name)
-      if self.transform:
-          image = self.transform(image)
+        image = cv2.imread(img_name)
+        if self.transform:
+            image = self.transform(image)
       
-      return image, label
+        return image, label
