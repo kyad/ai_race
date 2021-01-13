1c7d97ba82e7cc1ecec07e46639d778ce3de9723
kyad <53517611+kyad@users.noreply.github.com>
2021-01-13 14:38:30 +0000

    Add SampleNet2

--- ai_race/learning/scripts/samplenet.py
+++ ai_race/learning/scripts/samplenet.py
@@ -88,3 +88,40 @@ class SimpleNet(nn.Module):
         x = F.relu(x)
         x = self.fc1(x)
         return x
+
+class SimpleNet2(nn.Module):
+    def __init__(self):
+        super(SimpleNet2, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, 3, 1, padding=1)
+        self.conv2 = nn.Conv2d(16, 16, 3, 1, padding=1)
+        self.conv3 = nn.Conv2d(16, 16, 3, 1, padding=1)
+        self.conv4 = nn.Conv2d(16, 16, 3, 1, padding=1)
+        self.fc1 = nn.Linear(240, 16)
+        self.fc2 = nn.Linear(16, 3)
+        nn.init.kaiming_normal_(self.conv1.weight)
+        nn.init.kaiming_normal_(self.conv2.weight)
+        nn.init.kaiming_normal_(self.conv3.weight)
+        nn.init.kaiming_normal_(self.conv4.weight)
+        nn.init.kaiming_normal_(self.fc1.weight)
+        nn.init.kaiming_normal_(self.fc2.weight)
+
+    def forward(self, x):
+        x = nn.MaxPool2d(kernel_size=2)(x)
+        x = nn.MaxPool2d(kernel_size=2)(x)
+        x = nn.MaxPool2d(kernel_size=2)(x)
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = nn.MaxPool2d(kernel_size=2)(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = nn.MaxPool2d(kernel_size=2)(x)
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = nn.MaxPool2d(kernel_size=2)(x)
+        x = self.conv4(x)
+        x = F.relu(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        return x
