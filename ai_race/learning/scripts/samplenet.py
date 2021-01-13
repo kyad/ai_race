22808f010aafeaa55caaed3f592809e6ff9509d8
kyad <53517611+kyad@users.noreply.github.com>
2021-01-13 13:33:21 +0000

    Add use_gap

--- ai_race/learning/scripts/samplenet.py
+++ ai_race/learning/scripts/samplenet.py
@@ -33,16 +33,17 @@ class SampleNet(nn.Module):
         return x
 
 class SimpleNet(nn.Module):
-    def __init__(self, init_maxpool=1):
+    def __init__(self, init_maxpool=1, use_gap=True):
         super(SimpleNet, self).__init__()
         self.init_maxpool = init_maxpool
+        self.use_gap = use_gap
 
         self.conv1 = nn.Conv2d(3, 16, 3, 1, padding=1)
         self.conv2 = nn.Conv2d(16, 16, 3, 1, padding=1)
         self.conv3 = nn.Conv2d(16, 16, 3, 1, padding=1)
         self.conv4 = nn.Conv2d(16, 16, 3, 1, padding=1)
         self.conv5 = nn.Conv2d(16, 16, 3, 1, padding=1)
-        
+
         self.fc1 = nn.Linear(16, 3)
         nn.init.kaiming_normal_(self.conv1.weight)
         nn.init.kaiming_normal_(self.conv2.weight)
@@ -50,6 +51,16 @@ class SimpleNet(nn.Module):
         nn.init.kaiming_normal_(self.conv4.weight)
         nn.init.kaiming_normal_(self.conv5.weight)
         nn.init.kaiming_normal_(self.fc1.weight)
+
+        if not self.use_gap:
+            if self.init_maxpool == 1:
+                fc0_input_size = 1120
+            elif self.init_maxpool == 2:
+                fc0_input_size = 240
+            else:
+                raise NotImplementedError()
+            self.fc0 = nn.Linear(fc0_input_size, 16)
+            nn.init.kaiming_normal_(self.fc0.weight)
 
     def forward(self, x):
         for i in range(self.init_maxpool):
@@ -69,8 +80,11 @@ class SimpleNet(nn.Module):
         x = self.conv5(x)
         x = F.relu(x)
 
-        x = nn.AvgPool2d(kernel_size=x.size()[2:])(x)
+        if self.use_gap:
+            x = nn.AvgPool2d(kernel_size=x.size()[2:])(x)
         x = torch.flatten(x, 1)
+        if not self.use_gap:
+            x = self.fc0(x)
         x = F.relu(x)
         x = self.fc1(x)
         return x
